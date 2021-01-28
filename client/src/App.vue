442b99bb159cc1729c82678913e6d955ed061513
Afara Seun <seunvigo@gmail.com>
2021-01-28 18:35:10 +0100

    font fix

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,12 @@
 <template>
    <div id="app">
+      <div style="display:none">
+         <p style="font-family: 'OhDarling' !important;">I love you</p>
+         <p style="font-family: 'Andalusia' !important;">I love you</p>
+         <p style="font-family: 'VanillaDaisy' !important;">I love you</p>
+         <p style="font-family: 'angelova' !important;">I love you</p>
+         <p style="font-family: 'Lovefern' !important;">I love you</p>
+      </div>
       <div>
          <router-view />
       </div>
