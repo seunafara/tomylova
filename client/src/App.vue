d3da4e3b4dd25ec05a858891aee68d11782264ef
Afara Seun <seunvigo@gmail.com>
2021-01-28 18:41:42 +0100

    font fix

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,25 @@
 <template>
    <div id="app">
-      <div style="display:none">
+      <div>
+         <router-view />
+      </div>
+      <div id="font-loader">
          <p style="font-family: 'OhDarling' !important;">I love you</p>
          <p style="font-family: 'Andalusia' !important;">I love you</p>
          <p style="font-family: 'VanillaDaisy' !important;">I love you</p>
          <p style="font-family: 'angelova' !important;">I love you</p>
          <p style="font-family: 'Lovefern' !important;">I love you</p>
       </div>
-      <div>
-         <router-view />
-      </div>
    </div>
 </template>
 <script>
-export default {};
+export default {
+   mounted() {
+      window.onload = () => {
+         document.getElementById('font-loader').style.display = 'none';
+      };
+   },
+};
 </script>
 <style>
 @import './assets/css/style.css';
